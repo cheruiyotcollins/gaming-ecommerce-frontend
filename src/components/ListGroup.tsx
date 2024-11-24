58f2ca91404998b0f2dafdafd9d62436382a85bd
cheruiyotcollins <kelvincollins86@gmail.com>
2024-11-24 20:25:48 +0300

    feat: added react hook useState

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,4 +1,4 @@
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 function ListGroup() {
   let items = ["item1", "item2", "item3", "item4"];
   //   items = [];
@@ -6,14 +6,22 @@ function ListGroup() {
     return items.length === 0 && <p>No Items Found</p>;
   };
   const handleClick = (event: MouseEvent) => console.log(event);
-
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>List</h1>
       {getMessage()}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
