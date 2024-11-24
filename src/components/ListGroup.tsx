ae19b807166c757bf5e610c9b2a4139504b9d6c1
cheruiyotcollins <kelvincollins86@gmail.com>
2024-11-24 19:40:07 +0300

    feat: array loops and on click events on list of items

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,24 @@
+import { MouseEvent } from "react";
 function ListGroup() {
+  let items = ["item1", "item2", "item3", "item4"];
+  //   items = [];
+  const getMessage = () => {
+    return items.length === 0 && <p>No Items Found</p>;
+  };
+  const handleClick = (event: MouseEvent) => console.log(event);
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">An item</li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {getMessage()}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 export default ListGroup;
